c55963274c3b72ab051280e8a4c5ddc86e475e98
fpatel7 <79761974+fpatel7@users.noreply.github.com>
2021-02-28 22:15:44 -0600

    Add files via upload

---
+++ smile.py
@@ -0,0 +1,54 @@
+"""
+Name: smile.py
+Purpose : make smile face
+Author: Fenny Patel
+"""
+
+from turtle import*
+import math
+pensize(3)
+pu()
+bk(160)
+pd()
+circle(66)
+fillcolor("blue")
+begin_fill()
+circle(33)
+end_fill()
+
+
+from turtle import*
+import math
+pensize(3)
+pu()
+bk(160)
+pd()
+circle(66)
+fillcolor("blue")
+begin_fill()
+circle(33)
+end_fill()
+
+from turtle import*
+import math
+pensize(3)
+pu()
+bk(10)
+fd(120)
+lt(90)
+rt(40)
+pendown()
+pd()
+fillcolor("red")
+begin_fill()
+circle(33)
+end_fill()
+
+
+
+
+
+
+
+
+
